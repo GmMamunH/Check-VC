3c2155cfaa63c3f28f9d84e3375aed6e8ee04d8c
MD MAMUN HOSSAIN <gmmamun3004@gmail.com>
2023-12-10 23:54:44 +0600

    design change

--- alpha/index.js
+++ alpha/index.js
@@ -1,13 +1,45 @@
-const letter = prompt('Enter Your letter:');
+// const letter = prompt('Enter Your letter:');
 
-if (
+// if (
+//   letter === 'a' ||
+//   letter === 'e' ||
+//   letter === 'i' ||
+//   letter === 'o' ||
+//   letter === 'u'
+// ) {
+//   document.getElementById("result").innerHTML = letter + ' is Vowel';
+// } else {
+//     document.getElementById("per").innerHTML = letter + ' is consonant';
+// }
+
+const form = document.querySelector("form");
+const vowel = document.querySelector("#vowel");
+const consonant = document.querySelector("#consonant");
+
+form.addEventListener("submit", function(e){
+  e.preventDefault();
+  
+  const letter = document.querySelector("#letter").value;
+/* ==============================================
+  if (!letter) {
+    result.style.display = 'block';
+    result.innerText = 'কি ব্যপার উল্টা পাল্টা ইনপুট দেও কেন?';
+    this.reset();
+    return;
+  }
+  
+============================================== */
+
+ if (
   letter === 'a' ||
   letter === 'e' ||
   letter === 'i' ||
   letter === 'o' ||
   letter === 'u'
 ) {
-  document.getElementById("result").innerHTML = letter + ' is Vowel';
+  document.getElementById("consonant").innerHTML = letter + ' is Vowel';
 } else {
-    document.getElementById("per").innerHTML = letter + ' is consonant';
-}
+    document.getElementById("consonant").innerHTML = letter + ' is Consonant'};
+
+    consonant.style.display = 'block';
+});
